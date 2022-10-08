b03211d08db403edc2b749391a47b43f98497f51
Mahmoud Hashem <mahmoud.hashem34@outlook.com>
2022-10-08 04:39:46 +0200

    add argparse to gen_flow.py
    so you don't need to modify this file any more

--- gen_flow.py
+++ gen_flow.py
@@ -2,6 +2,20 @@ import cv2
 import numpy as np
 import os
 from natsort import natsorted
+
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--input_path", type=str)
+parser.add_argument("--npy_folder", type=str)
+parser.add_argument("--output_folder", type=str)
+
+args = parser.parse_args()
+
+
+
 def crop(image, center, radius, size=512):
     scale = 1.3
     radius_crop = (radius * scale).astype(np.int32)
@@ -25,9 +39,14 @@ def crop(image, center, radius, size=512):
 selected_joints = np.concatenate(([0,1,2,3,4,5,6,7,8,9,10], 
                     [91,95,96,99,100,103,104,107,108,111],[112,116,117,120,121,124,125,128,129,132]), axis=0) 
 
-folder = 'output_file_val' # 'output_file_train', 'output_file_test'
-npy_folder = 'val_npy/npy3' # 'train_npy/npy3', 'test_npy/npy3'
-out_folder = 'val_flow_depth' # 'train_flow_depth', 'test_flow_depth', 'train_flow_color', 'val_flow_color', 'test_flow_color' 
+# folder = 'output_file_val' # 'output_file_train', 'output_file_test'
+# npy_folder = 'val_npy/npy3' # 'train_npy/npy3', 'test_npy/npy3'
+# out_folder = 'val_flow_depth' # 'train_flow_depth', 'test_flow_depth', 'train_flow_color', 'val_flow_color', 'test_flow_color' 
+
+
+folder = args.input_path
+npy_folder = args.npy_folder
+out_folder = args.output_folder
 
 
 for o in natsorted(os.listdir(folder)):
